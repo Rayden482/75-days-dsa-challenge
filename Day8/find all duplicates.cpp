adc03ccce785cffb8af19f391e2bba590d659386
Parag kapoor <57284160+Rayden482@users.noreply.github.com>
2022-04-18 12:48:46 +0530

    Create find all duplicates.cpp

---
+++ Day8/find all duplicates.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(mp[nums[i]]==2){
+                ans.push_back(nums[i]);
+                mp[nums[i]]--;
+            }
+        }
+        return ans;
+    }
+};
